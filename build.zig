13788b041eb0e17006a6cf9f0096631e74aaa717
drzln <drzzln@protonmail.com>
2025-05-10 13:54:46 -0300

    init

--- build.zig
+++ build.zig
@@ -3,8 +3,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
     const zrpc = b.dependency("zrpc", .{});
-    const mach = b.dependency("mach", .{});
-    const clap = b.dependency("clap", .{});
     const exe = b.addExecutable(.{
         .name = "whiterun",
         .root_source_file = .{ .path = "src/main.zig" },
@@ -12,7 +10,5 @@ pub fn build(b: *std.Build) void {
         .optimize = mode,
     });
     exe.addModule("zrpc", zrpc.module("zrpc"));
-    exe.addModule("mach", mach.module("mach"));
-    exe.addModule("clap", clap.module("clap"));
     exe.install();
 }
