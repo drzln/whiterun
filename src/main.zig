e620b81d6f7f20bd713e65e3339f9c65f62547ba
drzln <drzzln@protonmail.com>
2025-05-10 14:06:40 -0300

    init

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const std  = @import("std");
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print(
-        "🏰  Whiterun CLI — Hello, Zig World!\n\
+        "🏰  Whiterun CLI — Hello, Zig World!\n
          (zrpc module imported, ready for future RPC magic)\n",
         .{},
     );
