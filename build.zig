9b9e5bda9a5a8b47275cbfdbc8c3356480521612
drzln <drzzln@protonmail.com>
2025-05-10 13:58:38 -0300

    init

--- build.zig
+++ build.zig
@@ -1,12 +1,9 @@
+// build.zig
 const std = @import("std");
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
     const exe = b.addExecutable(.{
         .name = "whiterun",
         .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = mode,
     });
-    exe.install();
+    b.installArtifact(exe);
 }
